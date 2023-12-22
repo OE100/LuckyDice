c5dc432902966f8eff100c943dcb5091fbe71db4
OE.omergunr100 <omer.er10@gmail.com>
2023-12-22 17:25:11 +0200

    Initial commit

---
+++ LuckyDice/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace LuckyDice
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private const string GUID = "oe.tweaks.luckydice";
+        private const string NAME = "Lucky Dice";
+        private const string VERSION = "1.0.0";
+
+        internal static Plugin Instance;
+
+        internal static BepInEx.Logging.ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = this.Logger;
+            Log.LogInfo($"'{NAME}' is loading...");
+
+            if (Instance == null)
+                Instance = this;
+            
+            harmony.PatchAll();
+
+            Log.LogInfo($"'{NAME}' loaded!");
+        }
+    }
+}
---
+++ LuckyDice/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LuckyDice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LuckyDice")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8756F0DB-69FB-4CB3-A6DF-46DC0BBA6E65")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
